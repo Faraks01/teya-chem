ee7930bbaa31d73edc6fe3d44737917364e6aecb
Konstantin Nikolaev <faraks01@gmail.com>
2023-04-10 02:28:44 +0600

    init

---
+++ src/theme.js
@@ -0,0 +1,10 @@
+import {createTheme} from "@mui/system";
+
+export const mainTheme = createTheme({
+  palette: {
+    purple: "#C67EFE",
+    blurple: "#3D3AD1",
+    gray93: "#EDEDED",
+    white: "#FFFFFF"
+  },
+});
