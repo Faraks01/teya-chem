4f8787eebf4a07e1ade6b0e7a3e1d995298ae5f3
Konstantin Nikolaev <faraks01@gmail.com>
2023-04-23 22:51:01 +0600

    Updated main theme

--- src/theme.js
+++ src/theme.js
@@ -6,6 +6,7 @@ export const mainTheme = createTheme({
       purple: "#C67EFE",
       blurple: "#3D3AD1",
       gray93: "#EDEDED",
+      gray72: "#B8B8B8",
       gray33: "#545454",
       lightGray: "#E2E2E2",
       nero: "#1E1E1E",
