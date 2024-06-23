28f33d82ca07a568db2e0105189b6279a87a7436
Konstantin Nikolaev <faraks01@gmail.com>
2024-06-23 23:46:59 +0300

    Fixed useQRCodeTransition hook

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,3 @@
 export * from "./api";
-export * as PATHS from "./router";
+export * from "./router";
 export * as PRODUCT from "./product";
--- src/constants/router.js
+++ src/constants/router.js
@@ -1,3 +1,3 @@
-export default {
+export const PATHS = {
   QR_TRANSITION: "/qr-transition",
 };
